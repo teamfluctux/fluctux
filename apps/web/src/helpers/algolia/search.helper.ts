c874692b5871f682d366780f13454a46aa7c861b
gitmahin <nimahin25@gmail.com>
2025-03-08 23:26:47 +0600

    added

--- apps/web/src/helpers/algolia/search.helper.ts
+++ apps/web/src/helpers/algolia/search.helper.ts
@@ -1,7 +1,26 @@
-import { liteClient as algoliasearch } from "algoliasearch/lite";
+import { liteClient as algoliasearch, LiteClient } from "algoliasearch/lite";
+import {
+  createNullCache,
+  createBrowserLocalStorageCache,
+  createMemoryCache,
+  createFallbackableCache,
+} from "@algolia/client-common";
 import { ALGOLIA_APPLICATION_ID, ALGOLIA_SEARCH_API } from "../common.helper";
 
-export const searchAlgolia = algoliasearch(
+export const searchAlgolia: LiteClient = algoliasearch(
   ALGOLIA_APPLICATION_ID || "",
-  ALGOLIA_SEARCH_API || ""
+  ALGOLIA_SEARCH_API || "",
+  {
+    requestsCache: createBrowserLocalStorageCache({
+      key: "algolia-search-cache",
+    }),
+    responsesCache: createFallbackableCache({
+      caches: [
+       
+        createMemoryCache(),
+        createNullCache(), 
+      ],
+    }),
+    hostsCache: createMemoryCache(),
+  }
 );
