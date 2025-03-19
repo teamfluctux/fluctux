5ef1b4c78ff11804d5b9923c23be8eb332c971f1
gitmahin <nimahin25@gmail.com>
2025-03-20 03:40:27 +0600

    updated

--- apps/web/src/components/core/docs/sidebar/doc-ondemand-sidebar.tsx
+++ apps/web/src/components/core/docs/sidebar/doc-ondemand-sidebar.tsx
@@ -10,7 +10,7 @@ const DynamicDocSidebar = dynamic<DocSidebarPropsType>(
     {
         ssr: false,
         loading: () => <aside
-            className={`w-[250px] h-screen sticky top-0 fx-primary-bg flex-shrink-0 doc-aside-nav transition-all duration-150 ease-out`}
+            className={`w-[250px] h-screen sticky top-0 fx-primary-bg flex-shrink-0 doc-aside-nav doc-aside-nav-loader transition-all duration-150 ease-out`}
         >
             <nav className="h-[calc(100%-105px)] sticky top-[105px] overflow-y-scroll custom-scrollbar pr-2 pb-16 doc-aside-nav-container">
                 <Skeleton height={52} borderRadius={"8px"} className='mb-1' />
