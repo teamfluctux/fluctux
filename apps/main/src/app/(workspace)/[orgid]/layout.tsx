beed6a7bd2ced7dafd470ab7d5e7f87ea7aa4840
gitmahin <nimahin25@gmail.com>
2025-03-22 14:06:24 +0600

    updated

--- apps/main/src/app/(workspace)/[orgid]/layout.tsx
+++ apps/main/src/app/(workspace)/[orgid]/layout.tsx
@@ -1,18 +1,23 @@
+'use client'
 import React from 'react'
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 interface WorkspaceLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
-export default function Layout({children}: WorkspaceLayoutProps) {
-  return  <PanelGroup direction="horizontal">
-  <Panel defaultSize={30} minSize={20}>
-    left
-  </Panel>
-  <PanelResizeHandle />
-  <Panel defaultSize={30} minSize={20}>
-    right
-  </Panel>
-</PanelGroup>
+export default function Layout({ children }: WorkspaceLayoutProps) {
+  return <PanelGroup direction="horizontal" id={"workspace-layout"}>
+    <Panel defaultSize={18} minSize={18} maxSize={25} >
+      <div className='w-full fx-primary-bg h-screen border-r fx-border-color'>
+
+      </div>
+    </Panel>
+    <PanelResizeHandle />
+    <Panel defaultSize={82}>
+      <div className='w-full fx-secondary-bg h-screen'>
+
+      </div>
+    </Panel>
+  </PanelGroup>
 }
