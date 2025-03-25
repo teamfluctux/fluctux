433d92ab3e79d5e844867425119190b193d0e180
gitmahin <nimahin25@gmail.com>
2025-03-25 22:03:49 +0600

    updated

--- packages/ui/src/fxbutton/button.tsx
+++ packages/ui/src/fxbutton/button.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 import { ROUNDED_VARIANTS } from "../constant";
 import { buttonSizes, ButtonVariant, getButtonStyling } from "./helper";
 
