fe97e96173ab40297ec796642e9a171bfa07b926
gitmahin <nimahin25@gmail.com>
2025-05-18 23:24:09 +0600

    updated

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,19 +7,38 @@ export default function HomePage() {
         <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[linear-gradient(to_right,var(--background-color-900C)1px,transparent_1px),linear-gradient(to_bottom,var(--background-color-900C)1px,transparent_1px)] bg-[size:6rem_6rem]"></div>
       </div> */}
 
+      <div className="absolute h-[800px] w-full top-0 left-0 overflow-hidden -z-10">
+        <div className="flex justify-center items-center w-full h-full rotate-[140deg]">
+          <div className="w-[100px] h-[calc(100vh+150%)] bg-[linear-gradient(85deg,_var(--background),_var(--background),_var(--background-color-900C))] "></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 11 bg-[linear-gradient(262deg,_var(--background-color-900C),_var(--background),_var(--background))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 10 bg-[linear-gradient(92deg,_var(--background),_var(--background),_var(--background-color-900C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 9 bg-[linear-gradient(63deg,_var(--background),_var(--background),_var(--background-color-900C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 8 bg-[linear-gradient(81deg,_var(--background),_var(--background),_var(--background-color-900C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 7 bg-[linear-gradient(96deg,_var(--background),_var(--background),_var(--background-color-850C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 6 bg-[linear-gradient(86deg,_var(--background),_var(--background),_var(--background-color-850C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 5 bg-[linear-gradient(94deg,_var(--background),_var(--background),_var(--background-color-900C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 4 bg-[linear-gradient(85deg,_var(--background),_var(--background),_var(--background-color-800C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 3 bg-[linear-gradient(112deg,_var(--background),_var(--background),_var(--background-color-900C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 2 bg-[linear-gradient(97deg,_var(--background),_var(--background),_var(--background-color-850C))]"></div>
+          <div className="w-[100px] h-[calc(100vh+150%)] 1 bg-[linear-gradient(84deg,_var(--background),_var(--background-color-900C))]"></div>
+        </div>
+
+        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-b from-transparent to-background-color_950C h-[300px]">
+
+        </div>
+      </div>
+
       <div className="fx-layout-max-1200 mx-auto h-[1000px]">
         <div className="w-full flex flex-col justify-center items-center mt-52">
-         
-            <h1 className="text-[55px] font-semibold text-center max-w-[900px] w-full leading-[3.6rem]">
-              Purpose-Built for Project Success and Team Connection
-            </h1>
-            <p className="text-[18px] font-medium text-text-color_2 text-center max-w-[800px] mt-5 leading-[1.5rem]">
-              Designed for your progress, Fluctux is the intuitive project
-              management and social collaboration platform that works the way
-              you do. Unite your team and effortlessly manage projects across
-              any industry
-            </p>
-     
+          <h1 className="text-[55px] font-semibold text-center max-w-[900px] w-full leading-[3.6rem]">
+            Purpose-Built for Project Success and Team Connection
+          </h1>
+          <p className="text-[18px] font-medium text-text-color_2 text-center max-w-[800px] mt-5 leading-[1.5rem]">
+            Designed for your progress, Fluctux is the intuitive project
+            management and social collaboration platform that works the way you
+            do. Unite your team and effortlessly manage projects across any
+            industry
+          </p>
         </div>
       </div>
     </div>
--- apps/web/src/components/core/header/header-auth-block.tsx
+++ apps/web/src/components/core/header/header-auth-block.tsx
@@ -1,42 +1,43 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import {
   FxButton,
   LUCIDE_WORKSPACE_ICON_SIZE,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
-
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@fluctux/ui";
-import { ChevronDown } from 'lucide-react';
-import Link from 'next/link';
-
+import { ChevronDown } from "lucide-react";
+import Link from "next/link";
 
 export const HeaderAuthBlock = () => {
   return (
     <div>
-          <div className="rounded overflow-hidden h-[35px] flex  justify-center items-center">
-            <FxButton className="pl-4 pr-2 h-full  font-medium text-workspace_1">
-              Sign up
+      <div className="rounded overflow-hidden h-[35px] flex  justify-center items-center">
+        <FxButton className="pl-4 pr-2 h-full  font-medium text-workspace_1">
+          Sign up
+        </FxButton>
+        <Popover>
+          <PopoverTrigger asChild className="outline-none">
+            <FxButton className="group h-full px-1">
+              <ChevronDown
+                size={LUCIDE_WORKSPACE_ICON_SIZE}
+                className="text-text-color_default_white"
+              />
             </FxButton>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild className="outline-none">
-                <FxButton className="group h-full px-1">
-                  <ChevronDown
-                    size={LUCIDE_WORKSPACE_ICON_SIZE}
-                    className="text-text-color_default_white"
-                  />
-                </FxButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-[105px] rounded" align="end">
-                <Link href={"#"}>
-                  <li className="text-workspace_1 font-medium text-text-color_1 cursor-pointer px-2 py-1 rounded-tiny list-none hover:bg-background-color_800C ">
-                    Login
-                  </li>
-                </Link>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </div>
-  )
-}
+          </PopoverTrigger>
+          <PopoverContent className="w-[103px] rounded h-[38px]" align="end">
+            <Link href={"#"}>
+              <li className="text-workspace_1 font-medium text-text-color_1 cursor-pointer px-2 h-full flex justify-start items-center  rounded-tiny list-none hover:bg-background-color_800C ">
+                Login
+              </li>
+            </Link>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+};
