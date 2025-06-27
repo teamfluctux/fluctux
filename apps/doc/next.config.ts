f8ea405c64dcd7beb15e520e203558763fc5e71d
gitmahin <nimahin25@gmail.com>
2025-06-27 13:15:58 +0600

    added doc project

---
+++ apps/doc/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // TODO: for docker image. otherwise comment this
+  output: "standalone",
+};
+
+export default nextConfig;
