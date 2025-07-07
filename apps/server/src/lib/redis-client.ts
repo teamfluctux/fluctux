9f3e35e35b3425e7958f7e515f6f7d8b3b8a0837
gitmahin <nimahin25@gmail.com>
2025-07-07 17:19:49 +0600

    add redis

---
+++ apps/server/src/lib/redis-client.ts
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+import fs from "fs"
+const redisClient = await createClient({
+    url: "",
+    // username: 'default', // use your Redis user. More info https://redis.io/docs/latest/operate/oss_and_stack/management/security/acl/
+    // password: 'secret', // use your password here
+    socket: {
+        host: 'my-redis.cloud.redislabs.com',
+        port: 6379,
+        tls: true,
+        key: fs.readFileSync('./redis_user_private.key'),
+        cert: fs.readFileSync('./redis_user.crt'),
+        ca: [fs.readFileSync('./redis_ca.pem')]
+    }
+})
+  .on("error", (err: any) => console.log("Redis Client Error", err))
+  .connect();
+
+export default redisClient
