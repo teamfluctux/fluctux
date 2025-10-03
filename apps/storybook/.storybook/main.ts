a1d43901de69feb7c0150102740dd3912e604f62
gitmahin <nimahin25@gmail.com>
2025-10-03 22:05:12 +0600

    update

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -16,7 +16,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-docs"),
     getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest"),
+    getAbsolutePath("@storybook/addon-vitest")
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -27,7 +27,7 @@ const config: StorybookConfig = {
       server: {
         fs: {
           // allow importing from the monorepo root
-          allow: ["../../"],
+          allow: ["../../", ".."],
         },
       },
     });
