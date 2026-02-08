4e6557ca62f2973732edf9440893470b83cf7c30
gitmahin <nimahin25@gmail.com>
2026-02-08 11:56:55 +0600

    remove commands folder

--- commands/package-build.sh
+++
@@ -1,9 +0,0 @@
-node --version
-npm --version
-npm install -g pnpm
-pnpm clean
-pnpm i
-pnpm --filter ui --filter hooks --filter constants build
-pnpm --filter zod build
-pnpm --filter shared build
-pnpm i
--- commands/start-all-webapps.sh
+++
@@ -1,5 +0,0 @@
-node --version
-npm --version
-npm install -g pnpm
-pnpm i
-pnpm run dev --filter doc --filter web --filter main --filter account 
