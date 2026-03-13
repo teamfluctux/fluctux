24fa7b8b11dc8dabd55df4721a9a89dec1f48c75
gitmahin <nimahin25@gmail.com>
2026-03-13 19:55:57 +0600

    style: add colors

--- packages/ui/src/scroll-area.tsx
+++ packages/ui/src/scroll-area.tsx
@@ -8,7 +8,7 @@ export const ScrollArea = ({ children, className }: ScrollAreaPropsType) => {
     <div
       className={`overflow-y-auto overflow-x-hidden [scrollbar-width:thin]
   [scrollbar-color:transparent_transparent]
-  hover:[scrollbar-color:var(--color-zinc-800)_transparent]
+  hover:[scrollbar-color:var(--background-color-800C)_transparent]
   transition-all ${className}`}
     >
       {children}
