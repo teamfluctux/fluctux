40487539238e6912d4c4514ed7cebfca9182b805
--list <nimahin25@gmail.com>
2026-04-28 15:10:52 +0600

    feat: add drag drop renderer comp ag-grid

--- templates/e-commerce-admin/src/components/ag-grid/AgCellPopoverWithCheckboxNDrag.tsx
+++ templates/e-commerce-admin/src/components/ag-grid/AgCellPopoverWithCheckboxNDrag.tsx
@@ -18,6 +18,68 @@ import {
 } from "@fluctux/ui";
 import type { ICellRendererParams } from "ag-grid-community";
 import React, { useState } from "react";
+import { RestrictToElement } from "@dnd-kit/dom/modifiers";
+import {
+  DragDropProvider,
+  DragOverlay,
+} from "@dnd-kit/react";
+import { RestrictToVerticalAxis } from "@dnd-kit/abstract/modifiers";
+
+import { isSortable, useSortable } from "@dnd-kit/react/sortable";
+
+function SortableDataList({
+  id,
+  index,
+  item,
+  isOverlay = false,
+  containerRef,
+}: {
+  id: string;
+  index: number;
+  isOverlay?: boolean;
+  item: AgCellPopoverWithCheckboxNDragDataType;
+  containerRef: React.RefObject<HTMLDivElement | null>;
+}) {
+  const { ref, isDragging } = useSortable({
+    id,
+    index,
+    // -- So that data can be pass through dndProvider
+    data: { item },
+    modifiers: [
+      // -- Restrict to specific container
+      RestrictToElement.configure({
+        element: containerRef.current ?? undefined,
+      }),
+    ],
+  });
+  const elementRef = React.useRef<HTMLDivElement>(null);
+
+  // -- Drag overlay should not contain this comp. This comp is for only if dragging is true then it will show in box overlay should get the default comp
+  if (isDragging && !isOverlay) {
+    return (
+      <div
+        style={{ width: `${elementRef.current?.offsetWidth}px` }}
+        className={cn(
+          "px-2  rounded-md border h-7 items-center flex  border-surface-border select-none text-workspace_2 font-medium cursor-default bg-surface-bg-active "
+        )}
+      ></div>
+    );
+  }
+
+  return (
+    <div
+      ref={(node) => {
+        ref(node);
+        elementRef.current = node; // ← attach both refs
+      }}
+      className={cn(
+        "px-2 rounded-md border bg-background-color_900C h-7 items-center flex border-border-color_1 w-fit select-none text-workspace_2 font-medium cursor-default hover:bg-background-color_800C"
+      )}
+    >
+      {item?.label}
+    </div>
+  );
+}
 
 type AgCellPopoverWithCheckboxNDragPropsType = {} & ICellRendererParams;
 export type AgCellPopoverWithCheckboxNDragDataType = MenuDataType & {};
@@ -31,6 +93,9 @@ export const AgCellPopoverWithCheckboxNDrag = (
   const handleAddData = (data: AgCellPopoverWithCheckboxNDragDataType) => {
     setSelectedData([...selectedData, data]);
   };
+  const [activeDragList, setActiveDragList] =
+    useState<AgCellPopoverWithCheckboxNDragDataType | null>(null);
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
   return (
     <Popover>
       <PopoverTrigger asChild className="w-full h-full px-4!">
@@ -57,18 +122,60 @@ export const AgCellPopoverWithCheckboxNDrag = (
         className="w-[500px] h-[350px] flex justify-center items-start z-[9999999999999]!"
       >
         <ScrollArea className="h-full border-r border-border-color_1 w-full">
-          <div className="w-full space-y-1 p-3">
-            {selectedData.map((item) => {
-              return (
-                <div
-                  className={cn(
-                    "px-2 py-1 rounded-md border border-border-color_1 w-fit select-none text-workspace_2 font-medium cursor-default hover:bg-background-color_800C"
-                  )}
-                >
-                  {item.label}
-                </div>
-              );
-            })}
+          <div className="w-full space-y-1 p-3" ref={containerRef}>
+            <DragDropProvider
+              modifiers={[RestrictToVerticalAxis]}
+              onDragStart={(event, manager) => {
+                const active = manager.dragOperation.source;
+                setActiveDragList(active?.data?.item ?? null);
+              }}
+              onDragEnd={(event) => {
+                if (event.canceled) return;
+
+                const { source } = event.operation;
+
+                if (isSortable(source)) {
+                  const { initialIndex, index } = source;
+
+                  if (initialIndex !== index) {
+                    setSelectedData((items) => {
+                      const newItems = [...items];
+                      const [removed] = newItems.splice(initialIndex, 1);
+                      newItems.splice(index, 0, removed!);
+                      return newItems;
+                    });
+                  }
+                }
+              }}
+            >
+              {selectedData.map((item, i) => {
+                return (
+                  <SortableDataList
+                    key={item.value}
+                    id={item.value}
+                    index={i}
+                    item={item}
+                    containerRef={containerRef}
+                  />
+                );
+              })}
+              <DragOverlay>
+                {(source) => {
+                  console.log("srouce is her mahin", source);
+                  return (
+                    <SortableDataList
+                      index={0}
+                      id={activeDragList?.value as string}
+                      isOverlay={true}
+                      containerRef={containerRef}
+                      item={
+                        activeDragList as AgCellPopoverWithCheckboxNDragDataType
+                      }
+                    />
+                  );
+                }}
+              </DragOverlay>
+            </DragDropProvider>
           </div>
         </ScrollArea>
         <ScrollArea className="h-full w-[550px]">
